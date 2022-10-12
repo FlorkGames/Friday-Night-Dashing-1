ceeec0c7871de41314aa4b273d2b8c4e1fd008e9
FlorkGames <105853494+FlorkGames@users.noreply.github.com>
2022-10-12 17:32:22 -0300

    Add files via upload

---
+++ assets/preload/stages/geometricalbg.lua
@@ -0,0 +1,65 @@
+function onCreate()
+	-- background shit
+	makeLuaSprite('back', 'backBG', -470,-800);
+	setScrollFactor('back', 0.9, 0.1);
+
+      makeAnimatedLuaSprite('grass','grassBG',-470,-900)addAnimationByPrefix('grass','dance','grassBG',24,true)
+      objectPlayAnimation('grass','dance',false)
+      setScrollFactor('grass', 0.9, 0.9);
+
+      makeAnimatedLuaSprite('scenario','scenarioBG',-390,-100)addAnimationByPrefix('scenario','dance','scenarioBG',24,true)
+      objectPlayAnimation('scenario','dance',false)
+      setScrollFactor('scenario', 1.0, 1.0);
+
+	makeLuaSprite('backgdfnf', 'backGDFNFBG', -560,-800);
+	setScrollFactor('backgdfnf', 1.0, 1.0);
+
+	makeLuaSprite('GDback', 'backGDBG', -470,-995);
+	setScrollFactor('GDback', 0.9, 0.9);
+
+	makeLuaSprite('dark', 'DarkBG', -500,-950);
+	setScrollFactor('dark', 0.9, 0.9);
+
+	makeLuaSprite('moredark', 'MoreDarkBG', -500,-900);
+	setScrollFactor('moredark', 0.9, 0.9);
+
+	-- sprites that only load if Low Quality is turned off
+	if not lowQuality then
+
+		makeAnimatedLuaSprite('robot','ultraviolenceRobot',1070,-900)addAnimationByPrefix('robot','dance','ultraviolenceRobot',24,true)
+      	objectPlayAnimation('robot','dance',false)
+      	setScrollFactor('robot', 0.9, 0.9);
+
+      	makeAnimatedLuaSprite('publicgeo','geometricalpublic',-470,-945)addAnimationByPrefix('publicgeo','dance','public',24,true)
+      	objectPlayAnimation('publicgeo','dance',false)
+      	setScrollFactor('publicgeo', 0.9, 0.9);
+
+		makeLuaSprite('REDbackgdfnf', 'REDbackGDFNFBG', -470,-1100);
+		setScrollFactor('backgdfnf', 0.4, 0.4);
+
+      end
+
+	addLuaSprite('REDbackgdfnf', false);
+	addLuaSprite('backgdfnf', false);
+	addLuaSprite('back', false);
+	addLuaSprite('robot', false);
+	addLuaSprite('grass', false);
+	addLuaSprite('publicgeo', false);
+	addLuaSprite('scenario', false);
+	addLuaSprite('GDback', false);
+      addLuaSprite('dark', false);
+      addLuaSprite('moredark', true);
+      
+      scaleObject('REDbackgdfnf', 2, 2);
+      scaleObject('backgdfnf', 2, 2);
+      scaleObject('back', 2, 2);
+      scaleObject('robot', 2, 2);
+      scaleObject('grass', 2, 2);
+      scaleObject('publicgeo', 2, 2);
+      scaleObject('scenario', 2, 2);
+      scaleObject('GDback', 2, 2);
+      scaleObject('dark', 2, 2);
+      scaleObject('moredark', 2, 2);
+	
+	close(true); --For performance reasons, close this script once the stage is fully loaded, as this script won't be used anymore after loading the stage
+end
